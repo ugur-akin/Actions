38a6c479f72caa67212b13d3a9718febd67cc6ac
Ugur Akin <ugurakinsoftware@gmail.com>
2022-02-09 20:32:14 +0300

    Turned octokit into a singleton interface
    (cherry picked from commit f36d6455d962e35d33b2c8d7d7d887bfa6476a83)

--- src/octokit-instance.ts
+++ src/octokit-instance.ts
@@ -1,4 +1,3 @@
-import * as core from '@actions/core';
 import * as github from '@actions/github';
 // This should be exported based on the docs:
 //  https://github.com/octokit/types.ts/tree/7e5dd312188253e962fa209b16963f78113ba8c3#get-response-types-from-endpoint-methods
@@ -18,85 +17,109 @@ export type IssueData = GetResponseDataTypeFromEndpointMethod<
   Octokit['rest']['issues']['get']
 >;
 
-let instance: Octokit;
-let owner: string;
-let repo: string;
-let pull_number: number;
+export interface IOctokitRest {
+  getPullRequest: () => Promise<PullRequestData>;
+  getPullRequestTemplate: () => Promise<string>;
+  postReview: (
+    summary: string,
+    event?: PullRequestEvent
+  ) => Promise<PullReviewData>;
+  getIssue: (issueNumber: number) => Promise<IssueData>;
+}
 
-const initialize = (
-  _owner: string,
-  _repo: string,
-  _pull_number: number
-): void => {
-  const auth = core.getInput('GITHUB_TOKEN', {required: true});
-  const octokit = github.getOctokit(auth);
-  instance = octokit;
-  owner = _owner;
-  repo = _repo;
-  pull_number = _pull_number;
-};
+const OctokitRest = (() => {
+  let instance: IOctokitRest;
+  let owner: string;
+  let repo: string;
+  let pull_number: number;
+  let isConfigured = false;
 
-const getPullRequest = async (): Promise<PullRequestData> => {
-  const {data: pullRequest} = await instance.rest.pulls.get({
-    owner,
-    repo,
-    pull_number,
-  });
-  return pullRequest;
-};
+  const config = (
+    _owner: string,
+    _repo: string,
+    _pull_number: number,
+    _token: string
+  ): IOctokitRest => {
+    if (isConfigured) {
+      throw new Error('Instance is already configured!');
+    }
 
-const getPullRequestTemplate = async (): Promise<string> => {
-  const {data: rawFileContents} = await instance.rest.repos.getContent({
-    owner,
-    repo,
-    path: 'pull_request_template.md',
-    mediaType: {
-      format: 'raw',
-    },
-  });
+    const octokit = github.getOctokit(_token);
 
-  if (typeof rawFileContents === 'string') {
-    return rawFileContents;
-  } else {
-    throw new Error(
-      `Unable to fetch contents of the template file as text. Type of response payload is ${typeof rawFileContents}, expected string.`
-    );
-  }
-};
+    owner = _owner;
+    repo = _repo;
+    pull_number = _pull_number;
+    isConfigured = true;
 
-const postReview = async (
-  summary: string,
-  event: PullRequestEvent = 'COMMENT'
-): Promise<PullReviewData> => {
-  try {
-    // TODO: implement comments
-    const {data: review} = await instance.rest.pulls.createReview({
-      owner,
-      repo,
-      pull_number,
-      event,
-      body: summary,
-    });
-    return review;
-  } catch (err) {
-    throw err;
-  }
-};
+    const getPullRequest = async (): Promise<PullRequestData> => {
+      const {data: pullRequest} = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number,
+      });
+      return pullRequest;
+    };
+    const getPullRequestTemplate = async (): Promise<string> => {
+      const {data: rawFileContents} = await octokit.rest.repos.getContent({
+        owner,
+        repo,
+        path: 'pull_request_template.md',
+        mediaType: {
+          format: 'raw',
+        },
+      });
+      if (typeof rawFileContents === 'string') {
+        return rawFileContents;
+      } else {
+        throw new Error(
+          `Unable to fetch contents of the template file as text. Type of response payload is ${typeof rawFileContents}, expected string.`
+        );
+      }
+    };
 
-const getIssue = async (issueNumber: number): Promise<IssueData> => {
-  const {data: pullRequest} = await instance.rest.issues.get({
-    owner,
-    repo,
-    issue_number: issueNumber,
-  });
-  return pullRequest;
-};
+    const getIssue = async (issueNumber: number): Promise<IssueData> => {
+      const {data: pullRequest} = await octokit.rest.issues.get({
+        owner,
+        repo,
+        issue_number: issueNumber,
+      });
+      return pullRequest;
+    };
 
-export default {
-  initialize,
-  getPullRequest,
-  getPullRequestTemplate,
-  postReview,
-  getIssue,
-};
-export {getPullRequest, getPullRequestTemplate, postReview};
+    const postReview = async (
+      summary: string,
+      event: PullRequestEvent = 'COMMENT'
+    ): Promise<PullReviewData> => {
+      try {
+        // TODO: implement comments
+        const {data: review} = await octokit.rest.pulls.createReview({
+          owner,
+          repo,
+          pull_number,
+          event,
+          body: summary,
+        });
+        return review;
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    instance = {getPullRequest, getPullRequestTemplate, postReview, getIssue};
+
+    return instance;
+  };
+
+  const getInstance = (): IOctokitRest => {
+    if (!isConfigured) {
+      throw new Error(
+        "Context isn't configured. Please call `.config()` before accessing the instance"
+      );
+    }
+    return instance;
+  };
+
+  return {config, getInstance};
+})();
+
+export default OctokitRest;
